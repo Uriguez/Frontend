b71801280183b47efcf09ddf03fbbfeea9ee7236
Uriel Castillo <uri61220@gmail.com>
2025-03-09 16:53:26 -0600

    Pagina web

---
+++ js/script.js
@@ -0,0 +1,445 @@
+// Objeto con las letras de las canciones
+const letrasCanciones = {
+"no_me_platiques_mas": `No me platiques más <br>
+Lo que debió pasar <br>
+Antes de conocernos <br>
+Sé que has tenido horas felices <br>
+Aún sin estar conmigo <br>
+No quiero ya saber <br>
+♪ <br>
+Qué pudo suceder <br>
+En todos estos años <br>
+Que tú has vivido con otras gentes <br>
+Lejos de mi cariño <br>
+Te quiero tanto que me encelo <br>
+Hasta de lo que pudo ser <br>
+Y me figuro que por eso <br>
+Es que yo vivo tan intranquilo <br>
+No me platiques ya <br>
+Déjame imaginar <br>
+Que no existe el pasado <br>
+Y que nacimos, el mismo instante <br>
+En que nos conocimos <br>
+♪ <br>
+Te quiero tanto que me encelo <br>
+Hasta de lo que pudo ser <br>
+Y me figuro que por eso <br>
+Es que yo vivo, tan intranquilo <br>
+No me platiques ya <br>
+Déjame imaginar <br>
+Que no existe el pasado <br>
+Y que nacimos, el mismo instante <br>
+En que nos conocimos,`,
+//-------------------------------------------------------------------------------------------
+
+"inolvidable": `En la vida hay amores que nunca <br>
+Pueden olvidarse <br>
+♪ <br>
+Imborrables momentos que siempre <br>
+Guarda el corazón <br>
+♪ <br>
+Pero aquello que un día nos hizo <br>
+Temblar de alegría <br>
+♪ <br>
+Es mentira que hoy pueda olvidarse <br>
+Con un nuevo amor <br>
+He besado otras bocas buscando <br>
+Nuevas ansiedades <br>
+Y otros brazos extraños me estrechan <br>
+Llenos de emoción <br>
+Pero solo consiguen hacerme <br>
+Recordar los tuyos <br>
+Que inolvidablemente vivirán en mí <br>
+♪ <br>
+(Inolvidable) <br>
+(Inolvidable) <br>
+♪ <br>
+Pero aquello que un día nos hizo <br>
+Temblar de alegría <br>
+♪ <br>
+Es mentira que hoy pueda olvidarse <br>
+Con un nuevo amor <br>
+He besado otras bocas buscando <br>
+Nuevas ansiedades <br>
+Y otros brazos extraños me estrechan <br>
+Llenos de emoción <br>
+Pero solo consiguen hacerme <br>
+Recordar los tuyos <br>
+Que inolvidablemente vivirán en mí <br>
+♪ <br>
+Que inolvidablemente vivirán en mí <br>
+♪ <br>
+(Inolvidable) <br>
+(Inolvidable) <br>
+(Inolvidable) <br>
+(Inolvidable) <br>
+(Inolvidable) <br>
+♪ <br>
+(Inolvidable...).` ,
+
+//-------------------------------------------------------------------------------------------
+
+"la_puerta": `La puerta se cerró detrás de ti <br>
+Y nunca más volviste a aparecer <br>
+Dejaste abandonada la ilusión <br>
+Que había en mi corazón por ti <br>
+La puerta se cerró detrás de ti <br>
+Y así, detrás de ti se fue mi amor <br>
+Creyendo que podría convencer <br>
+A tu alma de mi padecer <br>
+Pero es que no supiste soportar <br>
+Las penas que nos dio <br>
+La misma adversidad <br>
+Así como también <br>
+Nos dio felicidad <br>
+Nos vino a castigar con el dolor <br>
+La puerta se cerró detrás de ti <br>
+Y nunca más volviste a aparecer <br>
+Dejaste abandonada la ilusión <br>
+Que había en mi corazón por ti <br>
+♪ <br>
+Pero es que no supiste soportar <br>
+Las penas que nos dio <br>
+La misma adversidad <br>
+Así como también <br>
+Nos dio felicidad <br>
+Nos vino a castigar con el dolor <br>
+La puerta se cerró detrás de ti <br>
+Y nunca más volviste a aparecer <br>
+Dejaste abandonada la ilusión <br>
+Que había en mi corazón por ti ` ,
+
+//-------------------------------------------------------------------------------------------
+
+"la_barca": `Dicen que la distancia es el olvido <br>
+Pero yo no concibo esa razón <br>
+Porque yo seguiré siendo el cautivo <br>
+De los caprichos de tu corazón <br>
+Supiste esclarecer mis pensamientos <br>
+Me diste la verdad que yo soñé <br>
+Ahuyentaste de mí los sufrimientos <br>
+En la primera noche que te amé <br>
+Hoy mi playa se viste de amargura, oh <br>
+Porque tu barca tiene que partir <br>
+A cruzar otros mares de locura <br>
+Cuida que no naufrague en tu vivir <br>
+Cuando la luz del sol se esté apagando y <br>
+Te sientas cansada de vagar <br>
+Piensa que yo por ti estaré esperando <br>
+Hasta que tú decidas regresar <br>
+♪ <br>
+Supiste esclarecer mis pensamientos <br>
+Me diste la verdad que yo soñé <br>
+Ahuyentaste de mí los sufrimientos <br>
+En la primera noche que te amé <br>
+Hoy mi playa se viste de amargura <br>
+Porque tu barca tiene que partir <br>
+A cruzar otros mares de locura <br>
+Cuida que no naufrague en tu vivir <br>
+Cuando la luz del sol se esté apagando y <br>
+Te sientas cansada de vagar <br>
+Piensa que yo por ti estaré esperando <br>
+Hasta que tú decidas regresar <br>
+(Hasta que tú decidas regresar) `,
+
+//-------------------------------------------------------------------------------------------
+
+"teextraño": `Te extraño <br>
+Como se extrañan las noches sin estrellas <br>
+Como se extrañan las mañanas bellas <br>
+No estar contigo, por Dios que me hace daño <br>
+♪ <br>
+Te extraño <br>
+Cuando camino, cuando lloro, cuando río <br>
+Cuando el Sol brilla, cuando hace mucho frío <br>
+Porque te siento como algo muy mío <br>
+♪ <br>
+Te extraño <br>
+Como los árboles extrañan el otoño <br>
+En esas noches que no concilio el sueño <br>
+No te imaginas, amor, cómo te extraño <br>
+♪ <br>
+Te extraño <br>
+En cada paso que siento solitario <br>
+Cada momento que estoy viviendo a diario <br>
+Estoy muriendo, amor, porque te extraño <br>
+Te extraño <br>
+Cuando la aurora comienza a dar colores <br>
+Con tus virtudes, con todos tus errores <br>
+Por lo que quieras no sé, pero te extraño <br>
+♪ <br>
+Te extraño <br>
+En cada paso que siento solitario <br>
+Cada momento que estoy viviendo a diario <br>
+Estoy muriendo, amor, porque te extraño <br>
+Te extraño <br>
+Cuando la aurora comienza a dar colores <br>
+Con tus virtudes con todos tus errores <br>
+Por lo que quieras, no sé <br>
+Pero te extraño`,
+
+//-------------------------------------------------------------------------------------------
+
+"usted": `Usted es la culpable <br>
+De todas mis angustias <br>
+Y todos mis quebrantos <br>
+Usted llenó mi vida <br>
+De dulces inquietudes <br>
+Y amargos desencantos <br>
+Su amor es como un grito <br>
+Que llevo aquí en mi alma <br>
+Y aquí en mi corazón <br>
+Y soy, aunque no quiera <br>
+Esclavo de sus ojos <br>
+Juguete de su amor <br>
+No juegue con mis penas <br>
+Ni con mis sentimientos <br>
+Es lo único que tengo <br>
+Usted es mi esperanza <br>
+Mi última esperanza <br>
+Comprenda de una vez <br>
+Usted me desespera <br>
+Me mata, me enloquece <br>
+Y hasta la vida diera <br>
+Por vencer el miedo <br>
+De besarla a usted <br>
+♪ <br>
+Su amor es como un grito <br>
+Que llevo aquí en mi alma <br>
+Y aquí en mi corazón <br>
+Y soy, aunque no quiera <br>
+Esclavo de sus ojos <br>
+Juguete de su amor <br>
+No juegue con mis penas <br>
+Ni con mis sentimientos <br>
+Es lo único que tengo <br>
+Usted es mi esperanza <br>
+Mi última esperanza <br>
+Comprenda de una vez <br>
+Usted me desespera <br>
+Me mata, me enloquece <br>
+Y hasta la vida diera <br>
+Por vencer el miedo <br>
+De besarla a usted <br>
+Usted me desespera <br>
+Me mata, me enloquece <br>
+Y hasta la vida diera <br>
+Por vencer el miedo <br>
+De besarla <br>
+A usted`, 
+
+//-------------------------------------------------------------------------------------------
+
+"contigo": `No existe un momento del día <br>
+En que pueda apartarme de ti <br>
+El mundo parece distinto <br>
+Cuando no estás junto a mí <br>
+No hay bella melodía <br>
+♪ <br>
+En que no surjas tú <br>
+Ni yo quiero escucharla <br>
+Si no la escuchas tú <br>
+Es que te has convertido <br>
+En parte de mi alma <br>
+Ya nada me consuela <br>
+Si no estás tú también <br>
+Más allá de tus labios <br>
+Del sol y las estrellas <br>
+Contigo a la distancia <br>
+Amada mía, estoy <br>
+♪ <br>
+En parte de mi alma <br>
+Ya nada me consuela <br>
+Si no estás tú también <br>
+Más allá de tus labios <br>
+Del sol y las estrellas <br>
+Contigo a la distancia <br>
+Amada mía, estoy`,
+
+//-------------------------------------------------------------------------------------------
+
+"mucho_corazon": `Di si encontraste en mi pasado <br>
+Una razón para quererme <br>
+O para olvidarme <br>
+Pides cariño, pides olvido <br>
+Si te conviene <br>
+No llames corazón <br>
+Lo que tú tienes <br>
+De mi pasado preguntas todo <br>
+Que cómo fue <br>
+Si antes de amar <br>
+Debe tenerse fe <br>
+Dar por un querer <br>
+La vida misma sin morir <br>
+Eso es cariño <br>
+No lo que hay en ti <br>
+Yo para querer <br>
+No necesito una razón <br>
+Me sobra mucho <br>
+Pero mucho corazón <br>
+♪ <br>
+De mi pasado preguntas todo <br>
+Que cómo fue <br>
+Si antes de amar <br>
+Debe tenerse fe <br>
+Dar por un querer <br>
+La vida misma sin morir <br>
+Eso es cariño <br>
+No lo que hay en ti <br>
+Yo para querer <br>
+No necesito una razón <br>
+Me sobra mucho <br>
+Pero mucho corazón`,
+
+//-------------------------------------------------------------------------------------------
+
+"la_mentira": `Se te olvida, que me quieres a pesar de lo que dices <br>
+Pues llevamos, en el alma, cicatrices imposibles de borrar <br>
+♪ <br>
+Se te olvida que hasta puedo hacerte mal si me decido <br>
+Pues tu amor lo tengo muy comprometido <br>
+Pero a fuerza no será <br>
+♪ <br>
+Y hoy resulta que no soy de la estatura de tu vida <br>
+Y al dejarme casi, casi, se te olvida <br>
+Que hay un pacto entre los dos <br>
+♪ <br>
+Por mi parte te devuelvo tu promesa de adorarme <br>
+Ni siquiera sientas pena por dejarme <br>
+Que ese pacto no es con Dios <br>
+♪ <br>
+Y hoy resulta que no soy de la estatura de tu vida <br>
+Y al dejarme casi, casi, se te olvida <br>
+Que hay un pacto entre los dos <br>
+♪ <br>
+Por mi parte te devuelvo tu promesa de adorarme <br>
+Ni siquiera sientas pena por dejarme <br>
+Que ese pacto no es con Dios <br>
+♪ <br>
+Por mi parte te devuelvo tu promesa de adorarme <br>
+Ni siquiera sientas pena por dejarme <br>
+Que ese pacto no es con Dios, oh, no, no`, 
+
+//-------------------------------------------------------------------------------------------
+
+"cuando_vuelva_a_tu_lado": `Cuando vuelva a tu lado <br>
+No me niegues tus besos <br>
+Que el amor que te he dado <br>
+No podrás olvidar <br>
+No me preguntes nada <br>
+Que nada he de explicarte <br>
+Que el beso que negaste <br>
+Ya no lo puedes dar <br>
+Cuando vuelva a tu lado <br>
+Y esté solo contigo <br>
+Las cosas que te digo <br>
+No repitas jamás <br>
+Por compasión <br>
+Une tu labio al mío <br>
+Y estréchame en tus brazos <br>
+Y cuenta los latidos <br>
+De nuestro corazón <br>
+♪ <br>
+No me preguntes nada <br>
+Que nada he de explicarte <br>
+Que el beso que negaste <br>
+Ya no lo puedes dar <br>
+Cuando vuelva a tu lado <br>
+Y esté solo contigo <br>
+Las cosas que te digo <br>
+No repitas jamás <br>
+Por compasión <br>
+Une tu labio al mío <br>
+Y estréchame en tus brazos <br>
+Y cuenta los latidos <br>
+De nuestro corazón`,
+
+//-------------------------------------------------------------------------------------------
+
+"no_se_tu": `No sé tú <br>
+Pero yo <br>
+No dejo de pensar <br>
+Ni un minuto <br>
+Me logro despojar <br>
+De tus besos, tus abrazos <br>
+De lo bien que la pasamos <br>
+La otra vez <br>
+No sé tú <br>
+Pero yo <br>
+Quisiera repetir <br>
+El cansancio <br>
+Que me hiciste sentir <br>
+Con la noche que me diste <br>
+Y el momento <br>
+Que con besos construiste <br>
+No sé tú <br>
+Pero yo te he comenzado <br>
+A extrañar <br>
+En mi almohada no te dejo <br>
+De pensar <br>
+Con las gentes, mis amigos <br>
+En las calles <br>
+Sin testigos <br>
+No sé tú <br>
+Pero yo te busco en cada amanecer <br>
+Mis deseos no los puedo contener <br>
+En las noches, cuando duermo <br>
+Sin insomnio <br>
+Yo me enfermo <br>
+Me haces falta
+Mucha falta <br>
+No sé tú <br>
+♪ <br>
+No sé tú <br>
+Pero yo te busco en cada amanecer <br>
+Mis deseos no los puedo contener <br>
+En las noches, cuando duermo <br>
+Sin insomnio <br>
+Yo me enfermo <br>
+Me haces falta <br>
+Mucha falta <br>
+No sé tú`,
+
+//-------------------------------------------------------------------------------------------
+
+"como": `Cómo imaginar que la vida sigue igual <br>
+Cómo si tus pasos ya no cruzan el portal <br>
+Cómo pretender esta realidad <br>
+Cómo si hasta ayer brillaba el cielo en tu mirar. <br>
+Cómo consolar a la rosa y al jazmín <br>
+Cómo si tu risa ya no se oye en el jardín <br>
+Cómo he de mentirles que mañana volverás <br>
+Cómo despertar si tú no estás. <br>
+♪ <br>
+Cómo imaginar que la vida sigue igual <br>
+Cómo si tus pasos ya no cruzan el portal <br>
+Cómo pretender esta realidad <br>
+Cómo si hasta ayer brillaba el cielo en tu mirar <br>
+Cómo consolar a la rosa y al jazmín <br>
+Cómo si tu risa ya no se oye en el jardín <br>
+♪ <br>
+Cómo he de mentirles que mañana volverás <br>
+Cómo despertar si tú no estás.`, 
+
+//-------------------------------------------------------------------------------------------
+
+};
+
+// Función para mostrar u ocultar la letra
+function mostrarLetra(idCancion, event) {
+    event.preventDefault(); // Evita que la página se recargue o se desplace al inicio
+
+    let contenedor = document.getElementById(idCancion);
+
+    if (contenedor.classList.contains("oculto")) {
+        contenedor.innerHTML = `<h3>Letra de la canción</h3><p>${letrasCanciones[idCancion]}</p>`;
+        contenedor.classList.remove("oculto");
+    } else {
+        contenedor.classList.add("oculto");
+    }
+}
+
+function toggleMenu() {
+    let nav = document.getElementById("nav-menu");
+    nav.classList.toggle("mostrar");
+}
